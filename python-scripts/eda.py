4581fc1823973a4c89ea4a5cdc7eab249df7bfcb
Kerem Acar <kerem.acar789@gmail.com>
2024-07-11 15:13:27 -0700

    Adds rows variable

--- python-scripts/eda.py
+++ python-scripts/eda.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def data_analysis (file, message, x, y):
     df = pd.read_csv(file)
-    subset_df = df.iloc[:1000]
+    subset_df = df.iloc[:rows]
     plt.plot(subset_df[x], subset_df[y])
     plt.title(message)
     plt.show()
@@ -12,4 +12,6 @@ file = input('Enter file name: ')
 message = ''
 x = input('Enter x: ')
 y = input('Enter y: ')
+rows = int(input("How many rows do you want to show? "))
+
 data_analysis(file, message, x, y)
