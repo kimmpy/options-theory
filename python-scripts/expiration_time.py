ccdc0947bdcdbab1de76f1c369a95b9a68e5e9e6
Kim Pham <kim.kp.pham@gmail.com>
2024-07-15 10:14:35 -0700

    added time until expiration column

--- python-scripts/expiration_time.py
+++ python-scripts/expiration_time.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import datetime as dt
 
-def expiration (file):
-    df = pd.read_csv(file)
-    df['DATE'] = pd.to_datetime(df['DATE'])
-    df['EXPIRATION'] = pd.to_datetime(df['EXPIRATION'])
-    df['TIME-TO-EXPIRATION'] = df['EXPIRATION'] - df['DATE']
-    df['TIME-TO-EXPIRATION'] = df['TIME-TO-EXPIRATION'].dt.days
+def expiration(file):
+    df = pd.read_csv(file, parse_dates=['DATE', 'EXPIRATION'])
+    #df['DATE'] = pd.to_datetime(df['DATE'])
+    #df['EXPIRATION'] = pd.to_datetime(df['EXPIRATION'])
     
+    df['TIME-TO-EXPIRATION'] = (df['EXPIRATION'] - df['DATE']).dt.days
+    df.to_csv(file)
+
+
+file = input('Input file name: ')
+
+expiration(file)
--- python-scripts/string_to_datetime.py
+++ python-scripts/string_to_datetime.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 def string_to_datetime(file_name):
     df = pd.read_csv(file_name)
-    df['DATE'] = pd.to_datetime(df['DATE'], format='%m %d %Y')
+    df = df[df['EXPIRATION'].str.contains('2024')]
+    df['EXPIRATION'] = pd.to_datetime(df['EXPIRATION'], format='%m %d %Y')
     df.to_csv(file_name)
 
-file_name = ('Enter file name: ')
+file_name = input('Enter file name: ')
 string_to_datetime(file_name)
